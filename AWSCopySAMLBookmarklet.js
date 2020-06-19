3858f58c44eb43bd8c7cddd0bf859d67590ab578
Ryan Jaeger <rjjaeger@amazon.com>
2020-06-19 10:14:58 -0700

    fix formatting

--- AWSCopySAMLBookmarklet.js
+++ AWSCopySAMLBookmarklet.js
@@ -1,4 +1,14 @@
-(function () 
-{ 
-    function copyToClipboard(text) { var dummy = document.createElement("textarea"); document.body.appendChild(dummy); dummy.value = text; dummy.select(); document.execCommand("copy"); document.body.removeChild(dummy); } var saml = document.getElementsByName('SAMLResponse')[0]; console.log(saml.value); copyToClipboard(saml.value);
- })();
+(function () {
+    function copyToClipboard(text) { 
+        var dummy = document.createElement("textarea"); 
+        document.body.appendChild(dummy); 
+        dummy.value = text; 
+        dummy.select(); 
+        document.execCommand("copy"); 
+        document.body.removeChild(dummy); 
+    } 
+    
+    var saml = document.getElementsByName('SAMLResponse')[0]; 
+     
+    copyToClipboard(saml.value);
+})();
