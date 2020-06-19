f805ea08a786af970652367dabdd422c499cde54
Ryan Jaeger <rjjaeger@amazon.com>
2020-06-19 10:18:43 -0700

    update bookmarket.html

--- AWSCopySAMLBookmarklet.js
+++ AWSCopySAMLBookmarklet.js
@@ -1,3 +1,7 @@
+// ==Bookmarklet==
+// @name AWS Copy SAML Bookmarklet
+// @author Ryan Jaeger (@rjjaegeraws) , Shea Phillips (@sheaphillips)
+// ==/Bookmarklet==
 (function () {
     function copyToClipboard(text) { 
         var dummy = document.createElement("textarea"); 
